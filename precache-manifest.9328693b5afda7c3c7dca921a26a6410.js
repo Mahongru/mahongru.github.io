ddc0cb2cd3b7499bd6ae457fe6dd6b0b04445789
mahongru <mahongru.ca@gmail.com>
2018-10-20 16:11:12 -0700

    Updates

---
+++ precache-manifest.9328693b5afda7c3c7dca921a26a6410.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "348c6081e575bf73040d",
+    "url": "/mahongru.github.io/static/js/runtime~main.348c6081.js"
+  },
+  {
+    "revision": "07b74695695db19a7d56",
+    "url": "/mahongru.github.io/static/js/main.07b74695.chunk.js"
+  },
+  {
+    "revision": "b99f27b3cb84157e8901",
+    "url": "/mahongru.github.io/static/js/1.b99f27b3.chunk.js"
+  },
+  {
+    "revision": "88f2d30e4f6be9228f699324f368175c",
+    "url": "/mahongru.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f128925fa2766a844ecdada19a53016f.js"
+  "/mahongru.github.io/precache-manifest.9328693b5afda7c3c7dca921a26a6410.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/mahongru.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
